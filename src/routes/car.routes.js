9eabf57000b0dc61fa0d59c4c83d15337d07bb19
jpablomaure <jpablomaure@gmail.com>
2022-09-07 10:59:21 -0300

    primera entrega proyecto final

--- src/routes/car.routes.js
+++ src/routes/car.routes.js
@@ -13,18 +13,26 @@ carRouter.get('/', async (req, res) =>{
     res.json((await carritosApi.listarAll()))
 });
 
+// guardar un producto al carrito 
+
 carRouter.post('/', async (req, res) =>{
     res.json({ id: await carritosApi.guardar({productos: [] }) });
 });
+
+// borrar un producto del carrito 
 
 carRouter.delete('/:id', async (req, res) => {
     res.json(await carritosApi.borrar(req.params.id))
 });
 
+// listar productos del carrito
+
 carRouter.get('/:id/productos', async (req, res) => {
     const carrito = await carritosApi.listar(req.params.id);
     res.json(carrito.productos);
 });
+
+// agregar un producto al carritos
 
 carRouter.post('/:id/productos', async (req, res) =>{
     const carrito = await carritosApi.listar(req.params.id);
@@ -34,6 +42,8 @@ carRouter.post('/:id/productos', async (req, res) =>{
     res.end();
 
 });
+
+// borrar un producto del carrito 
 
 carRouter.delete('/:id/productos/:idProd', async (req, res) => {
     const carrito = await carritosApi.listar(req.params.id);
